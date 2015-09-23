16c9e912ccbed4e261ded510aa3f4c47c3c68f8e
Chris Antaki <ChrisAntaki@gmail.com>
2015-09-23 11:08:30 -0700

    Calling out missing organization tag

--- count.js
+++ count.js
@@ -1,6 +1,11 @@
 var HashCount = require('./src/HashCount');
 
-var credoCount = new HashCount({
+if (!process.argv[2]) {
+    console.log('Please enter an organization tag. Ex: credo');
+    process.exit();
+}
+
+new HashCount({
     organization: process.argv[2],
     organizations: require('./input/organizations'),
 });
